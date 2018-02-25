6be39fc1fda6b163a7df2530aad8dd35ce6928bb
Minh <hungquocminh1989@gmail.com>
2018-02-25 20:39:50 +0700

    add obfuscator-javascript

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,5 @@
 <?php 
+define('SYSTEM_BASE_URL', 'http://127.0.0.1/simple_src/');
 define('DATABASE_DNS', 'mysql:dbname=test1;host=45.76.180.116');
 define('DATABASE_USER', 'root');
 define('DATABASE_PASS', 'pass5510453');
--- app/config/define.php
+++ app/config/define.php
@@ -5,4 +5,4 @@ define('SMARTY_RIGHT_DELIMITER', '%}');
 define('SYSTEM_DEFAULT_TIMEZONE', 'Asia/Tokyo');
 define('SYSTEM_PUBLIC_DIR', str_replace("\\", '/', getcwd()));
 define('SYSTEM_VIEW_JS', str_replace("\\", '/', getcwd()).'/app/views/js');
-define('SYSTEM_JS_ENCRYTION', TRUE);
+define('SYSTEM_JS_ENCRYTION', FALSE);
