67dc751797289dfc1f302d9607b0e619eeaa13b3
Minh_Vnit <hungquocminh1989@gmail.com>
2018-03-28 18:25:56 +0700

    Update FW

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,8 @@
 <?php 
 define('SYSTEM_BASE_URL', 'http://127.0.0.1/simple_src/');
-define('DATABASE_DNS', 'mysql:dbname=test1;host=45.76.180.116');
-define('DATABASE_USER', 'root');
+define('DATABASE_TYPE', 'pgsql');
+define('DATABASE_NAME', 'sdh_2018');
+define('DATABASE_HOST', '45.76.180.116');
+define('DATABASE_USER', 'postgres');
 define('DATABASE_PASS', 'pass5510453');
+define('DATABASE_DNS', sprintf ('%1$s:dbname=%2$s;host=%3$s',DATABASE_TYPE, DATABASE_NAME, DATABASE_HOST));
